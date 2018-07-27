678612fb097877c66b6797ded048e61744281892
wangpeng <wangpeng022@sina.cn>
2018-07-27 18:36:03 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'copyqq'
+  }
+</script>
+
+<style>
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    -webkit-app-region: drag;
+  }
+  .iconfont{
+    color: rgb(170, 174, 190);
+    font-size: 12px;
+  }
+  .a:hover{
+    color: pink;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="wrapper">
+    <div class="head">
+      <div class="btns">
+        <i class="iconfont icon-jiantouxia"></i>
+        <i class="iconfont icon-zuixiaohua1"></i>
+        <i class="iconfont icon-guanbi"></i>
+      </div>
+    </div>
+    <main>
+
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    components: {  },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  #wrapper {
+    background:
+      /* radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      ); */
+      #c6c6c6;
+    height: 330px;
+    width: 430px;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .head{
+    width: 100%;
+    height: 180px;
+    background: url("../../../static/imgs/loginLogo.png") no-repeat;
+    background-size: 100% 100%;
+  }
+  .btns{
+    float: right;
+    padding: 10px;
+    cursor: default;
+    -webkit-app-region: no-drag;
+  }
+  .btns i{
+    padding: 0 4px;
+    z-index: 2;
+  }
+  .btns i:hover{
+    color: #fff;
+  }
+</style>
